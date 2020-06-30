b81060bbd8964b2249e5856f49f09e5407c00847
Adriano Zorzi <adizorzi@unochapeco.edu.br>
2020-06-30 00:13:29 -0300

    category form component

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service"
+
+import toaster from "toastr";
+import { FormStyle } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
@@ -7,9 +16,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryFormComponent implements OnInit {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAferContentChacked(){
+    this.setPageTitle();
+  }
+
+  private setCurrentAction() {
+    if(this.route.snapshot.url[0].path == 'new'){
+      this.currentAction = "new";
+    }else{
+      this.currentAction = "edit";
+    }
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: null
+    });
+  }
+
+  private loadCategory() {
+    if(this.currentAction == "edit") {
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get("id")))
+      )
+      .subscribe(
+        (category) => {
+          this.category = category
+          this.categoryForm.patchValue(category)
+        },
+        (error) => alert("Ocorreu um erro no Servidor!!")
+      )
+    }
+  }
+
+  private setPageTitle() {
+    if(this.currentAction == 'new') {
+      this.pageTitle = 'Cadastro de Nova Categoria'
+    }else{
+      const categoryName = this.category.name || ''
+      this.pageTitle = 'Editando Categoria:' + categoryName;
+    }
+  }
 }
